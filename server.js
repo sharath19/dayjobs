16b7180fb934d8b36095ea59e0c7f5205bd4c208
sharath reddy <reddy.sharath19@gmail.com>
2016-11-21 03:43:13 +0000

    add routes

--- server.js
+++ server.js
@@ -30,3 +30,39 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
     console.log("App now running on port", port);
   });
 });
+
+
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+/*  "/contacts"
+ *    GET: finds all contacts
+ *    POST: creates a new contact
+ */
+app.get("/", function(req, res) {
+  res.render("index")
+});
+
+
+app.get("/contacts", function(req, res) {
+});
+
+app.post("/contacts", function(req, res) {
+});
+
+/*  "/contacts/:id"
+ *    GET: find contact by id
+ *    PUT: update contact by id
+ *    DELETE: deletes contact by id
+ */
+
+app.get("/contacts/:id", function(req, res) {
+});
+
+app.put("/contacts/:id", function(req, res) {
+});
+
+app.delete("/contacts/:id", function(req, res) {
+});
